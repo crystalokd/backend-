aa6949adbe006c265c3a4dbef8805d77b63ec8ff
crystal <crystalokd@gmail.com>
2025-01-29 17:20:51 +0100

    add cors headers 003

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ app.add_middleware(
 def read_root():
     current_time = datetime.now(pytz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ")  # Exact ISO format
     return {
-        "email": "your-email@example.com",
+        "email": "crystalokd@gmail.com",
         "current_datetime": current_time,
-        "github_url": "https://github.com/yourusername/your-repo"
+        "github_url": "https://github.com/crystalokd/backend-"
     }
+
+            
+        

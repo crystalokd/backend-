1ccb616885cca38d2f78e186fa0fa06cd4525248
crystal <crystalokd@gmail.com>
2025-01-29 16:57:01 +0100

    add cors headers

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import pytz
 
 app = FastAPI()
 
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows requests from any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+
 @app.get("/")
 def read_root():
     return {
